0d6d8abbc6c4905d075c5a2f9978621eafd69f5d
WadeZimmerleUCF <wa575681@ucf.edu>
2026-04-03 18:25:41 -0400

    Update server.js

--- skilltree-backend/server.js
+++ skilltree-backend/server.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors())
 const User = require('./models/User');
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
