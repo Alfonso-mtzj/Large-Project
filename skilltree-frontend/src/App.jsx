861f1a2cb80c098a5a422cb5218ebe7e8176d80a
AlyssaDambeck <al667195@ucf.edu>
2026-04-02 22:51:51 -0400

    Update App.jsx
    added dashboard redirect

--- skilltree-frontend/src/App.jsx
+++ skilltree-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Dashboard from './pages/Dashboard'; 
 
 function App() {
   return (
