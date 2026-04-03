62429c3aa8163c7abbc198499d75d87e59a94f9a
WadeZimmerleUCF <wa575681@ucf.edu>
2026-04-03 18:57:03 -0400

    Update auth.js

--- skilltree-frontend/src/api/auth.js
+++ skilltree-frontend/src/api/auth.js
@@ -3,4 +3,4 @@ import axios from 'axios';
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 export const registerUser = (formData) => API.post('/api/register', formData);
-export const loginUser    = (formData) => API.post('/api/Login', formData);
+export const loginUser    = (formData) => API.post('/api/login', formData);
