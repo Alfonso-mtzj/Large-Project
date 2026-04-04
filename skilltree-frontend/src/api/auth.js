b3633131a52b5b012bfc5d99f7cdf95d7ec9e048
AlyssaDambeck <al667195@ucf.edu>
2026-04-03 23:56:07 -0400

    Update auth.js

--- skilltree-frontend/src/api/auth.js
+++ skilltree-frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'mongodb+srv://admin:<db_password>@cluster0.8mpvvxu.mongodb.net/?appName=Cluster0' });
 
 export const registerUser = (formData) => API.post('/api/register', formData);
 export const loginUser    = (formData) => API.post('/api/login', formData);
