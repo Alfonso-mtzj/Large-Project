5c842940344164efced6cfb6daa4441aa7081b84
AlyssaDambeck <al667195@ucf.edu>
2026-04-04 00:31:39 -0400

    Update auth.js

--- skilltree-frontend/src/api/auth.js
+++ skilltree-frontend/src/api/auth.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+/*
 const baseURL = import.meta.env.VITE_API_URL || 'http://lifexpskilltree.xyz/api/register';
 
 const API = axios.create({
@@ -8,8 +9,13 @@ const API = axios.create({
     'Content-Type': 'application/json',
   },
 });
+*/
+
+const API = axios.create({
+  baseURL: 'http://134.209.72.180:5000'
+});
 
 export const registerUser = (formData) => API.post('/api/register', formData);
 export const loginUser = (formData) => API.post('/api/login', formData);
 
-export default API;
+//export default API;
