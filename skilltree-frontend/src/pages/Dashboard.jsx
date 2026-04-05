42e7b7e4db2664566dabf672808578fd845f0721
AlyssaDambeck <al667195@ucf.edu>
2026-04-05 00:01:25 -0400

    Update Dashboard.jsx
    added user

--- skilltree-frontend/src/pages/Dashboard.jsx
+++ skilltree-frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard()
   return (
     <div style= {{ textAlign: 'center, marginTop: '50px' }}>
       <h1>Dashboard</h1>
-      <p>Welcome {user?.email}</p>
+      <p>Welcome {user?.email || "User"}</p>
     </div>
   );
 }
