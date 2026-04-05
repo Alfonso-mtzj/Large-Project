6bb01a1e91036f68bb550ccd4b61af6ef932adee
AlyssaDambeck <al667195@ucf.edu>
2026-04-04 23:52:27 -0400

    Create Dashboard.jsx
    made a small dashboard page for now

---
+++ skilltree-frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard()
+{
+  return (
+    <div style= {{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>DASHBOARD</h1>
+      <p>You are logged in congratsss!!!</p>
+    </div>
+    );
+}
