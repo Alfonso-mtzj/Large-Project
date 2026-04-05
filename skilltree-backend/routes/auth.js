d54b9e4a3fb183a4dbd05af5c231ecf14409e8d1
Alfonso <al570757@ucf.edu>
2026-04-05 02:40:02 -0400

    Update auth.js

--- skilltree-backend/routes/auth.js
+++ skilltree-backend/routes/auth.js
@@ -42,7 +42,7 @@ router.post('/register', async (request, response) => {
         });
 
         // 🔥 Send email via SendGrid
-        const verificationLink = `${process.env.CLIENT_URL}/verify/${tokenVerify}`;
+      const verificationLink = `https://lifexpskilltree.xyz/api/users/verify/${tokenVerify}`;
 
         await sgMail.send({
             to: email,
