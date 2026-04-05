2f57be0cfa210cb31656f2ca821dd1f7f9912f03
AlyssaDambeck <al667195@ucf.edu>
2026-04-04 22:33:58 -0400

    Update server.js

--- skilltree-backend/server.js
+++ skilltree-backend/server.js
@@ -7,7 +7,10 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://lifexpskilltree.xyz",
+  credentials: true
+}));
 
 // routes
 const authRoutes = require('./routes/auth');
