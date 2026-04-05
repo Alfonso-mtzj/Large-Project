f5c4509edeb7f724c22dc46a66739a56202b30a5
AlyssaDambeck <al667195@ucf.edu>
2026-04-05 00:21:19 -0400

    Update App.jsx
    fixed import

--- skilltree-frontend/src/App.jsx
+++ skilltree-frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { AuthProvider } from './context/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-import Verify from './pages/Veify'}
+import Verify from './pages/Verify';
 
 function App() {
   return (
