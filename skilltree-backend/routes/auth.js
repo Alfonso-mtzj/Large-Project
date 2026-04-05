429c4b8eb92b35ce9d6abdf53624bfaf90614979
AlyssaDambeck <al667195@ucf.edu>
2026-04-05 01:41:02 -0400

    Update auth.js
    fixing gmail stupid stuff

--- skilltree-backend/routes/auth.js
+++ skilltree-backend/routes/auth.js
@@ -6,10 +6,15 @@ const nodemailer = require('nodemailer');
 const User = require('../models/User');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, //important cuz its gotta be false for 587
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false
     },
     family: 4 //forces IPv4
 });
