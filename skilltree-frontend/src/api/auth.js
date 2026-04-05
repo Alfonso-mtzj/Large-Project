a92f76b74a6288cc5e631feecf2a889ab3410b94
AlyssaDambeck <al667195@ucf.edu>
2026-04-04 22:12:22 -0400

    Update auth.js
    local testing

--- skilltree-frontend/src/api/auth.js
+++ skilltree-frontend/src/api/auth.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://lifexpskilltree.xyz';
+//const baseURL = import.meta.env.VITE_API_URL || 'http://lifexpskilltree.xyz';
+const baseURL = 'http://localhost:5000';
 
 const API = axios.create({
   baseURL,
