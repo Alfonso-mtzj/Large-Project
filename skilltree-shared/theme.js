4211eab0c804fbe3aa543b7dea1f933a6e0009f8
Grace-1019 <gr969275@ucf.edu>
2026-04-08 10:41:21 -0400

    Update theme.js

--- skilltree-shared/theme.js
+++ skilltree-shared/theme.js
@@ -24,3 +24,12 @@ export const FONTS = {
   bodySemiBold: 'CrimsonText_600SemiBold',
   regular: 'Cinzel_400Regular',
 };
+
+export const SPACING = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+  xxl: 48,
+};
