207590c1281a1ca1ee996d029e83c59f5f5748cc
Grace-1019 <gr969275@ucf.edu>
2026-04-16 19:15:20 -0400

    Delete skilltree-mobile directory

--- skilltree-mobile/src/api/client.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// Expo uses EXPO_PUBLIC_* variables
-const baseURL = process.env.EXPO_PUBLIC_API_URL || 'http://lifexpskilltree.xyz';
-
-export const api = axios.create({
-  baseURL,
-  headers: { 'Content-Type': 'application/json' }
-});
