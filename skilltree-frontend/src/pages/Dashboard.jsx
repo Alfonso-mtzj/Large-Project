98ce544f9c3332061374b1bcaf9de9c949a4cf4c
AlyssaDambeck <al667195@ucf.edu>
2026-04-17 18:02:01 -0400

    Update Dashboard.jsx
    moved nav bar into background so it removed the white space and the gap

--- skilltree-frontend/src/pages/Dashboard.jsx
+++ skilltree-frontend/src/pages/Dashboard.jsx
@@ -60,13 +60,15 @@ export default function Dashboard() {
 
   return (
     <>
-      {/* NAV */}
-      <div className="navBar">
-        <button className="navButton" onClick={() => navigate('/dashboard')}>🧙 Home</button>
-        <button className="navButton" onClick={() => navigate('/calendar')}>📅 Calendar</button>
-      </div>
 
       <div className="dashboardBg" style={{ backgroundImage: `url(${bg})` }}>
+        
+        {/* NAV */}
+        <div className="navBar">
+          <button className="navButton" onClick={() => navigate('/dashboard')}>🧙 Home</button>
+          <button className="navButton" onClick={() => navigate('/calendar')}>📅 Calendar</button>
+        </div>
+        
         <div className="dashboardFrame">
 
           <div className="characterPanel">
