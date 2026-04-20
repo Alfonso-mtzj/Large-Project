4a98c156806195a07918c8150324994430503087
AlyssaDambeck <al667195@ucf.edu>
2026-04-19 21:59:14 -0400

    Update Dashboard.jsx

--- skilltree-frontend/src/pages/Dashboard.jsx
+++ skilltree-frontend/src/pages/Dashboard.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 export default function Dashboard() {
   const { user } = useAuth();
   const navigate = useNavigate();
+  console.log("USER IN DASHBOARD:", user);
 
   //intelligence
   const [studyHours, setStudyHours] = useState('');
